32bc9506992efaa0d0c47c6097e0a3d699198fe6
patrikkusnir1 <patrikkusnir1@gmail.com>
2023-09-06 08:10:39 +0300

    34 lesson string immutability

---
+++ 34_str_immut.js
@@ -0,0 +1,14 @@
+// Understand String Immutability
+
+let myStr = "Bob";
+myStr[0] = "J"; // it doesn't change anything 
+
+let myStr = "Bob";
+myStr = "Job"; // reassigning it
+
+// Correct the assignment to myStr so it contains the string value of Hello World using the approach shown in the example above.
+
+let myStr = "Jello World";
+
+myStr = "Hello World";
+
