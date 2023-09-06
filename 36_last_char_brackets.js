afd42412f45c0c7a42e64e8a3cb4af54e22d08b3
patrikkusnir1 <patrikkusnir1@gmail.com>
2023-09-06 08:19:28 +0300

    last letter finding lesson

---
+++ 36_last_char_brackets.js
@@ -0,0 +1,9 @@
+// Use Bracket Notation to Find the Last Character in a String
+
+const firstName = "Ada";
+firstName[firstName.length - 1]; // last character .length - 1
+
+// Use bracket notation to find the last character in the lastName variable.
+
+const lastName = "Lovelace";
+const lastLetterOfLastName = lastName[lastName.length - 1]
