6c0842495666426e86326257245b0f1be25e39e6
patrikkusnir1 <patrikkusnir1@gmail.com>
2023-09-06 08:23:14 +0300

    nth to last character lesson 37

---
+++ 37_nth_tolast.js
@@ -0,0 +1,9 @@
+// Use Bracket Notation to Find the Nth-to-Last Character in a String
+
+const firstName = "Augusta";
+firstName[firstName.length - 3] // it would be s
+
+// Use bracket notation to find the second-to-last character in the lastName string.
+
+const lastName = "Lovelace";
+const secondToLastLetterOfLastName = lastName[lastName.length - 2] 
