bfa297e695356460f0f89130b6a8a9ceafda7b30
patrikkusnir1 <patrikkusnir1@gmail.com>
2023-09-06 07:57:54 +0300

    add string length lesson

---
+++ 32_string_length.js
@@ -0,0 +1,14 @@
+// Find the Length of a String - .length 
+
+console.log("Alan Peter".length) // 10 will be result
+const firstName = "Ada";
+firstName.length // length property of the string variable
+
+// Use the .length property to set lastNameLength to the number of characters in lastName.
+
+let lastNameLength = 0;
+const lastName = "Lovelace";
+
+// Only change code below this line
+lastNameLength = lastName.length;
+
