b9ff7a101cb88f43eff2dfc28c71d33e6af775c7
patrikkusnir1 <patrikkusnir1@gmail.com>
2023-09-06 08:35:57 +0300

    added comment

--- 39_arrays.js
+++ 39_arrays.js
@@ -2,4 +2,5 @@
 
 const sandwich = ["peanut butter", "jelly", "bread"];
 
+// Modify the new array myArray so that it contains both a string and a number (in that order).
 const myArray = ["two", 2, "flat", "job"]
