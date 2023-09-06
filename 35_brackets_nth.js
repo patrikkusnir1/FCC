cae9e1a97e4fec19a820d2c55a3774f40388a403
patrikkusnir1 <patrikkusnir1@gmail.com>
2023-09-06 08:14:43 +0300

    finding nth character with brackets

---
+++ 35_brackets_nth.js
@@ -0,0 +1,8 @@
+// Use Bracket Notation to Find the Nth Character in a String
+
+const firstName = "Ada";
+const secondLetterOfFirstName = firstName[1];
+console.log(secondLetterOfFirstName) // console gives d 
+
+const lastName = "Lovelace";
+const thirdLetterOfLastName = lastName[2];
