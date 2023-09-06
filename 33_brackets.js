b4d500f6f12f53c7e6d478fb46d51209245d3163
patrikkusnir1 <patrikkusnir1@gmail.com>
2023-09-06 08:05:18 +0300

    brackets notation - first letter finding

---
+++ 33_brackets.js
@@ -0,0 +1,13 @@
+// Use Bracket Notation to Find the First Character in a String
+
+const firstName = "Charles";
+const firstLetter = firstName[0];
+console.log(firstLetter) // C
+
+// Use bracket notation to find the first character in the lastName variable and assign it to firstLetterOfLastName.
+
+let firstLetterOfLastName = "";
+const lastName = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName[0]
