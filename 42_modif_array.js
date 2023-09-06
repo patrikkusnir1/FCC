18035e329a82f558d722a8a365273b1a1b422d15
patrikkusnir1 <patrikkusnir1@gmail.com>
2023-09-06 20:59:35 +0300

    Modify Array Data With Indexes

---
+++ 42_modif_array.js
@@ -0,0 +1,9 @@
+// Modify Array Data With Indexes
+const ourArray = [50, 40, 30];
+ourArray[0] = 15;
+ourArray; // 15, 40, 30
+
+const myArray = [18, 64, 99];
+// Modify the data stored at index 0 of myArray to a value of 45.
+
+myArray[0] = 45
