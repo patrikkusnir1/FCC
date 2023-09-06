2efadcf415683278aa285396da693b38ed52eaa5
patrikkusnir1 <patrikkusnir1@gmail.com>
2023-09-06 20:55:28 +0300

    added semicolon

--- 41_array_data_index.js
+++ 41_array_data_index.js
@@ -5,5 +5,5 @@ console.log(array[0]); // 50
 const data = array[1]; // 60
 
 //Create a variable called myData and set it to equal the first value of myArray using bracket notation.
-const myArray = [50, 60, 70]
-let myData = myArray[0]
+const myArray = [50, 60, 70];
+let myData = myArray[0];
