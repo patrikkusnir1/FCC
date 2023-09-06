abe3e43b662ff1c0d24a5459238da37f2919ddc9
patrikkusnir1 <patrikkusnir1@gmail.com>
2023-09-06 20:55:00 +0300

    accessing array data with indexes

---
+++ 41_array_data_index.js
@@ -0,0 +1,9 @@
+// Access Array Data with Indexes
+
+const array = [50, 60, 70];
+console.log(array[0]); // 50
+const data = array[1]; // 60
+
+//Create a variable called myData and set it to equal the first value of myArray using bracket notation.
+const myArray = [50, 60, 70]
+let myData = myArray[0]
