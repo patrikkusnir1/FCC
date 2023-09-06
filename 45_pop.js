a0e1edd8cf2f54ff6b8055afac3d8314898b8615
patrikkusnir1 <patrikkusnir1@gmail.com>
2023-09-06 21:41:23 +0300

    Manipulate Arrays With pop Method

---
+++ 45_pop.js
@@ -0,0 +1,11 @@
+// Manipulate Arrays With pop Method
+
+const threeArr = [1, 4, 6];
+const oneDown = threeArr.pop();
+console.log(oneDown); // 6 - removes last element
+console.log(threeArr); // [1, 4];
+
+// Use the .pop() function to remove the last item from myArray and assign the popped off value to a new variable, removedFromMyArray.
+
+const myArray = [["John", 23], ["cat", 2]];
+const removedFromMyArray = myArray.pop();
