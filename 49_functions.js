20b29380a14c14ed3e2a0fb0e406f4d1ba98b94d
patrikkusnir1 <patrikkusnir1@gmail.com>
2023-09-07 08:14:22 +0300

    learn about functions

---
+++ 49_functions.js
@@ -0,0 +1,14 @@
+// Write Reusable JavaScript with Functions
+
+function functionName() {
+ console.log("Hello World")
+}
+
+// Create a function called reusableFunction which prints the string Hi World to the dev console.
+// Call the function.
+
+function reusableFunction() {
+ console.log("Hi World")
+}
+
+reusableFunction(); // call the function
