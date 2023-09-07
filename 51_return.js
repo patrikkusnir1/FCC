324c70e0df42aadfefddbab751e9a1b1a811307d
patrikkusnir1 <patrikkusnir1@gmail.com>
2023-09-07 20:05:59 +0300

    return function

---
+++ 51_return.js
@@ -0,0 +1,11 @@
+// Return a Value from a Function with Return
+
+function plusThree(num) {
+ return num + 3
+}
+
+const answer = plusThree(5); // 8
+// Create a function timesFive that accepts one argument, multiplies it by 5, and returns the new value.
+function timesFive(num) {
+ return num * 5;
+}
