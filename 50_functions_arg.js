d3f8179c246851d43b46c28a38bb070d180a744f
patrikkusnir1 <patrikkusnir1@gmail.com>
2023-09-07 08:20:39 +0300

    functions with arguments

---
+++ 50_functions_arg.js
@@ -0,0 +1,18 @@
+// Passing Values to Functions with Arguments
+
+function testFun(param1, param2) {
+ console.log(param1, param2);
+}
+
+testFun("Hello", "World");
+
+/* 
+Create a function called functionWithArgs that accepts two arguments and outputs their sum to the dev console.
+Call the function with two numbers as arguments.
+*/
+
+function functionWithArgs(num1, num2) {
+ console.log(num1 + num2) // 2 numbers sum
+}
+
+functionWithArgs(2, 2); // call function
