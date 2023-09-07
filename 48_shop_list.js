d98ca2eca5b21db7ebf1eb81e49b44145227fa74
patrikkusnir1 <patrikkusnir1@gmail.com>
2023-09-07 08:10:40 +0300

    shopping list create

---
+++ 48_shop_list.js
@@ -0,0 +1,18 @@
+// Shopping list
+
+/* 
+Create a shopping list in the variable myList. The list should be a multi-dimensional array containing several sub-arrays.
+
+The first element in each sub-array should contain a string with the name of the item. The second element should be a number representing the quantity i.e.
+
+["Chocolate Bar", 15]
+There should be at least 5 sub-arrays in the list.
+*/
+
+const myList = [
+ ["milk", 3],
+ ["eggs", 5],
+ ["chicken breast", 2],
+ ["bread", 1],
+ ["mineral water", 4]
+];
