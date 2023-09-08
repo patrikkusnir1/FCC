a5934b60be1169a03fadee562f676108587641b4
patrikkusnir1 <patrikkusnir1@gmail.com>
2023-09-08 23:31:04 +0300

    equity operator

---
+++ 60_equity.js
@@ -0,0 +1,19 @@
+// Comparison with the Equality Operator
+
+function equalityTest(myVal) {
+ if (myVal == 10) { //if myVal is true, equal, if not - not equal, works also "10"
+  return "Equal"
+ }
+ return "Not Equal"
+}
+
+// Add the equality operator to the indicated line so that the function will return the string Equal when val is equivalent to 12.
+
+function testEqual(val) {
+ if (val == 12) {
+  return "Equal"
+ }
+ return "Not equal"
+}
+
+testEqual(10)
