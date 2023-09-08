663dac7efc2e6617378b6d877fd740cb7a67ae77
patrikkusnir1 <patrikkusnir1@gmail.com>
2023-09-08 23:07:30 +0300

    return in function

--- 56_return.js
+++ 56_return.js
@@ -5,3 +5,14 @@ function sum(a, b) {
 }
 
 ourSum = sum(5, 12);
+ourSum; // returns 17
+
+// Call the processArg function with an argument of 7 and assign its return value to the variable processed.
+
+let processed = 0;
+
+function processArg(num) {
+ return (num + 3) / 5;
+}
+
+processed = processArg(7)
