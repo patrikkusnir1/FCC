1beeb748b18d7dde594370da53ad3c8b3ea55283
patrikkusnir1 <patrikkusnir1@gmail.com>
2023-09-11 08:09:39 +0300

    chaining if else statements

---
+++ 74_chaining if else.js
@@ -0,0 +1,26 @@
+// Chaining If Else Statements
+
+/*Write chained if/else if statements to fulfill the following conditions:
+
+num < 5 - return Tiny
+num < 10 - return Small
+num < 15 - return Medium
+num < 20 - return Large
+num >= 20 - return Huge
+
+*/
+function testSize(num) {
+ if (num < 5) {
+  return "Tiny";
+ } else if (num < 10) {
+  return "Small";
+ } else if (num < 15) {
+  return "Medium";
+ } else if (num < 20) {
+  return "Large";
+ } else if (num >= 20) {
+  return "Huge"
+ }
+}
+
+testSize(7)
