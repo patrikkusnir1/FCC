b61133430f003f77f7387bb018d022df9e9b5394
patrikkusnir1 <patrikkusnir1@gmail.com>
2023-09-18 08:17:25 +0300

    replacing if else

---
+++ 78_mult_opt_switch.js
@@ -0,0 +1,45 @@
+// Multiple Identical Options in Switch Statements
+
+let result = ""; 
+switch (val) {
+    case 1:
+    case 2:
+    case 3:
+        result = "1, 2 or 3";
+        break;
+    case 4:
+        result = "4 alone"
+}
+
+/* Write a switch statement to set answer for the following ranges:
+1-3 - Low
+4-6 - Mid
+7-9 - High
+
+Note: You will need to have a case statement for each number in the range. */
+
+function sequentialSizes(val) {
+    let answer = ""
+    switch(val) {
+        case 1:
+        case 2:
+        case 3:
+            answer = "Low";
+            break
+
+        case 4:
+        case 5:
+        case 6:
+            answer = "Mid";
+            break
+        case 7:
+        case 8:
+        case 9:
+            answer = "High"
+            break;
+    }
+    return answer
+    
+}
+
+sequentialSizes(1)
