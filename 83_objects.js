458e000c86555a98ba874c4a5b95f2e8575e1966
patrikkusnir1 <patrikkusnir1@gmail.com>
2023-09-24 13:46:10 +0300

    objects

---
+++ 83_objects.js
@@ -0,0 +1,26 @@
+const cat = {
+    "name": "Whiskers",
+    "legs": 4,
+    "tails": 1,
+    "enemies": ["Water", "Dogs"]
+}
+
+const anotherObject = {
+    make: "Ford",
+    5: "five",
+    "model": "focus"
+};
+
+/* 
+Make an object that represents a dog called myDog which contains the properties name (a string), legs, tails and friends.
+
+You can set these object properties to whatever values you want, as long as name is a string, legs and tails are numbers, and friends is an array.
+
+*/ 
+
+const myDog = {
+    name: "Beny",
+    legs: 4, 
+    tails: 1,
+    friends: ["dad", "neighbour"]
+}
