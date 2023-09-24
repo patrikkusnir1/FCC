8f3ce872750b5bbb895b64efb85500140bfcb4cf
patrikkusnir1 <patrikkusnir1@gmail.com>
2023-09-24 20:27:08 +0300

    objects dots

---
+++ 84_objects_dot.js
@@ -0,0 +1,22 @@
+const myObj = {
+    prop1: "val1",
+    prop2: "val2"
+};
+
+const prop1val = myObj.prop1; //val1
+const prop2val = myObj.prop2; // val2
+console.log(prop2val)
+
+/* 
+Read in the property values of testObj using dot notation. Set the variable hatValue equal to the object's property hat and set the variable shirtValue equal to the object's property shirt.
+*/
+
+const testObj = {
+    "hat": "ballcap",
+    "shirt": "jersey",
+    "shoes": "cleats"
+}
+
+const hatValue = testObj.hat;
+const shirtValue = testObj.shirt;
+console.log(hatValue, shirtValue)
