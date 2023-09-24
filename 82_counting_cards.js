24fe0287ff5eab0373cbfa3a3e15420a538644dc
patrikkusnir1 <patrikkusnir1@gmail.com>
2023-09-24 13:41:16 +0300

    cards counting

---
+++ 82_counting_cards.js
@@ -0,0 +1,21 @@
+let count = 0;
+
+function cc(card) {
+    if (card >= 2 && card <= 6) {
+        count++;
+    }
+    else if (card === 10  ||
+            card === "J" ||
+            card === "Q" ||
+            card === "K" ||
+            card === "A") {
+            count--;
+            }
+    if (count > 0) return count +  " Bet" // add just count + Bet
+    return count + " Hold"
+        }
+cc(2);
+cc(3);
+cc(7);
+cc("K");
+cc("A");
