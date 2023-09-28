c5aa8c4c04e290895f9ba4866fc8fb3b527dc1b6
patrikkusnir1 <78038878+patrikkusnir1@users.noreply.github.com>
2023-09-28 14:08:40 +0200

    for loop

---
+++ 97_forloop.js
@@ -0,0 +1,16 @@
+// Iterate with JavaScript For Loops
+
+const ourArray = [];
+for (let i = 0; i < 5; i++) {
+    ourArray.push(i)
+}
+console.log(ourArray) // [0, 1, 2, 3, 4]
+
+// Use a for loop to push the values 1 through 5 onto myArray.
+
+const myArray = [];
+for (let i = 1; i <= 5; i++) {
+    myArray.push(i)
+}
+
+console.log(myArray);
