346766760f95efedaa5c3e89ceea80f3ecf75e36
patrikkusnir1 <78038878+patrikkusnir1@users.noreply.github.com>
2023-09-28 12:40:16 +0200

    complex object data

---
+++ 92_complex_obj.js
@@ -0,0 +1,43 @@
+// Manipulating Complex Objects
+
+const ourMusic = [ 
+{   
+   "artist": "Daft Punk",
+    "title": "Homework",
+    "release_year": 1997,
+    "formats": [
+        "CD",
+        "Cassette",
+        "LP"
+    ],
+    "gold": true
+    }
+];
+
+
+// Add a new album to the myMusic array. Add artist and title strings, release_year number, and a formats array of strings.
+
+const myMusic = [
+    {
+        "artist": "Billy Joel",
+        "title": "Piano Man",
+        "release_year": 1973,
+        "formats": [
+            "CD",
+            "8t",
+            "LP"
+        ],
+        "gold": true
+    },
+    // new album
+    {
+        "artist": "Vaso Patejdl",
+        "title": "Nepoznana", 
+        "release_year": 1980, 
+        "formats": [
+            "CD",
+            "DVD",
+            "BlueRay"
+        ]
+    }
+]
