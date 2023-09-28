ab08ce08df59aad7157236d35b8dd027b1f15fc6
patrikkusnir1 <78038878+patrikkusnir1@users.noreply.github.com>
2023-09-28 14:17:50 +0200

    forloop counting backwards

---
+++ 99_forloop.js
@@ -0,0 +1,16 @@
+// Count Backwards With a For Loop
+
+const ourArray = [];
+
+for (let i = 10; i > 0; i -= 2) {
+    ourArray.push(i)
+}
+console.log(ourArray) // [ 10, 8, 6, 4, 2 ]
+
+// Push the odd numbers from 9 through 1 to myArray using a for loop.
+
+const myArray = [];
+for (let i = 9; i > 0; i -= 2) {
+    myArray.push(i)
+}
+console.log(myArray)
