ca9cf0de8a95926e129f26de6d1fc9fb4805cbfd
patrikkusnir1 <78038878+patrikkusnir1@users.noreply.github.com>
2025-10-16 23:33:49 +0300

    create random sentence game

--- curriculum/index.js
+++ curriculum/index.js
@@ -1,36 +1,48 @@
 /* 
-You should log "Hello! I'm your coding fun fact guide!" to the console as a greeting message to the user.
-You should create three variables: botName, botLocation, and favoriteLanguage, that store the bot's name, where it's from, and its favorite coding language, respectively.
-You should log "My name is (botName) and I live on (botLocation)." to the console.
-You should log "My favorite programming language is (favoriteLanguage)." to the console.
-You should use let to create a codingFact variable and assign it a string that is a fun fact about your bot's favorite coding language and include the use of the favoriteLanguage variable.
-You should log the codingFact to the console.
-You should reassign the codingFact variable to a new fact about the bot's favorite language using the favoriteLanguage variable again.
-You should log the codingFact to the console again.
-You should reassign the codingFact variable again to another new fact about the bot's favorite language using the favoriteLanguage variable.
-You should log the codingFact to the console a third time.
-You should log "It was fun sharing these facts with you. Goodbye! - (botName) from (botLocation)." to the console as a farewell statement from the bot.
-*/
 
-console.log("Hello! I'm your coding fun fact guide!");
-const botName = "JS bot";
-const botLocation = "JS Planet";
-const favoriteLanguage = "Javascript";
-console.log("My name is " + botName + " and I live on " + botLocation + ".");
-console.log("My favorite programming language is " + favoriteLanguage + ".");
-let codingFact = "I was created by Patrik," + favoriteLanguage + " specialist.";
-console.log(codingFact);
-codingFact =
-  "I chose " +
-  favoriteLanguage +
-  " because I thought, it will be easy to learn.";
-console.log(codingFact);
-codingFact = favoriteLanguage + " is my passion!";
-console.log(codingFact);
-console.log(
-  "It was fun sharing these facts with you. Goodbye! - " +
-    botName +
-    " from " +
-    botLocation +
-    "."
-);
+
+    You should declare the following variables using let:
+        adjective
+        noun
+        verb
+        place
+        adjective2
+        noun2
+
+    You should assign the above variables some string values of your choice.
+
+    You should declare a firstStory variable.
+
+    You should use the following story template to create the first story and assign it to the firstStory variable: 
+    "Once upon a time, there was a(n) [adjective] [noun] who loved to eat [noun2]. The [noun] lived in a [place] and had [adjective2] nostrils that blew fire when it was [verb].";
+
+    You should output your first story to the console using the message "First story: [firstStory]".
+
+    You should assign new values to your adjective, noun, verb, place, adjective2, and noun2 variables.
+
+    You should declare a secondStory variable.
+
+    Create another story using the same template and assign it to the secondStory variable.
+
+    You should output your second story to the console using the message "Second story: [secondStory]".
+
+*/
+let adjective = "nice";
+let noun = "goat";
+let verb = "sleep";
+let place = "Moscow";
+let adjective2 = "ugly";
+let noun2 = "city";
+
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb; 
+console.log("First story: " + firstStory);
+
+adjective = "beautiful";
+noun = "house";
+verb = "walks";
+place = "school";
+adjective2 = "good-looking";
+noun2 = "pickles";
+
+let secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb; 
+console.log("Second story: " + secondStory);
