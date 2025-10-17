1f55f17bda5676478cc219c361869437ae128769
patrikkusnir1 <78038878+patrikkusnir1@users.noreply.github.com>
2025-10-17 16:17:28 +0300

    learn slice method

--- curriculum/index.js
+++ curriculum/index.js
@@ -1,6 +1,9 @@
-let letter = "A";
-console.log(letter.charCodeAt(0));
-let symbol = "!";
-console.log(symbol.charCodeAt(0));
-let char = String.fromCharCode(66);
-console.log(char)
+let message = "I love Javascript!";
+let greeting = message.slice(0, 5);
+console.log(greeting)
+let world = message.slice(7);
+console.log(world)
+let lastWord = message.slice(-4);
+console.log(lastWord)
+let language = message.slice(7, 17);
+console.log(language)
