dee865bc08a4e786788f9b1a29940560abf0c2ab
patrikkusnir1 <78038878+patrikkusnir1@users.noreply.github.com>
2025-10-19 15:45:19 +0300

    create getLoanMessage function

--- curriculum/email masker/index.js
+++ curriculum/email masker/index.js
@@ -1,12 +1,18 @@
 const maskEmail = email => {
+  // find where domain starts
   const domainPosition = email.indexOf("@");
-  const domain = email.slice(domainPosition);
+  
+  // split email into 2 parts
   const name = email.slice(0, domainPosition);
-  console.log(name);
+  const domain = email.slice(domainPosition);
 
   // keep first and last character, mask the middle
-  const maskedName = name[0] + "*".repeat(name.length - 2) + name[name.length - 1];
+  const maskedName = 
+    name[0] + "*".repeat(name.length - 2) + name[name.length - 1];
+
+  // combine masked name with domain
   return maskedName + domain;
 }
+// example
 const email = "user@domain.org";
 console.log(maskEmail(email))
