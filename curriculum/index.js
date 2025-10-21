1398c12a1fc42b9450c6deefa2fbdbac477b6644
patrikkusnir1 <78038878+patrikkusnir1@users.noreply.github.com>
2025-10-21 19:42:04 +0300

    learn about two dimensional arrays

--- curriculum/index.js
+++ curriculum/index.js
@@ -1,22 +1,12 @@
+let chessboard = [
+  ["R", "N", "B", "Q", "K", "B", "K", "R"],
+  ["P", "P", "P", "P", "P", "P", "P", "P"],
+  [" ", " ", " ", " ", " ", " ", " ", " "],
+  [" ", " ", " ", " ", " ", " ", " ", " "],
+  [" ", " ", " ", " ", " ", " ", " ", " "],
+  [" ", " ", " ", " ", " ", " ", " ", " "],
+  ["p", "p", "p", "p", "p", "p", "p", "p"],
+  ["r", "n", "b", "q", "k", "b", "n", "p"]
+];
 
-let globalVar = "I'm a global variable"; // ? global variable
-
-function printGlobalVar() {
-  console.log(globalVar) // * use inside the function
-}
-
-printGlobalVar()
-
-function greet() {
-  let message = "Hello, local scope!"; // ? local variable
-  console.log(message);
-}
-
-greet();
-//console.log(message) //! throws an error
-
-if (true) {
-  let blockVar = "I'm in a block";
-  console.log(blockVar);
-}
-console.log(blockVar); //! throws an error
+console.log(chessboard[0][3])
