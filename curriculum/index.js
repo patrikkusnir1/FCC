125a8a7e49251c70fd015184f5a3e2b972cc6b56
patrikkusnir1 <78038878+patrikkusnir1@users.noreply.github.com>
2025-10-24 19:46:33 +0300

    learn about shallow copy

--- curriculum/index.js
+++ curriculum/index.js
@@ -1,3 +1,7 @@
-let array = [1, 2, 3, 4, 5];
-array.splice(0);
-console.log(array)
+const originalArray = [1, 2, 3];
+const copyArray = [...originalArray];
+copyArray.push(4);
+console.log(originalArray);
+console.log(copyArray)
+let array = [];
+console.log(array.shift());
