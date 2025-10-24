276e5084101e01c0d6fe3ec0997aae5c52b6ba6b
patrikkusnir1 <78038878+patrikkusnir1@users.noreply.github.com>
2025-10-24 19:53:47 +0300

    learn objects as basics

--- curriculum/index.js
+++ curriculum/index.js
@@ -1,7 +1,8 @@
-const originalArray = [1, 2, 3];
-const copyArray = [...originalArray];
-copyArray.push(4);
-console.log(originalArray);
-console.log(copyArray)
-let array = [];
-console.log(array.shift());
+const person = {
+  name: "Alice",
+  age: 30,
+  city: "Wonderland"
+}
+
+let propertyName = "city";
+console.log(person[propertyName]);
