dd141eec4e40d4888af30b6162e0faa06e3b45a4
patrikkusnir1 <78038878+patrikkusnir1@users.noreply.github.com>
2025-10-25 11:05:35 +0300

    create recipe tracker

--- curriculum/index.js
+++ curriculum/index.js
@@ -1,19 +1,6 @@
-const user = {
-  name: "John",
-  age: 30,
-  isAdmin: true
+function createPerson(name, age) {
+  return {name, age}
 }
 
-// const jsonString = JSON.stringify(user);
-
-const developerObj = {
-  firstName: "Jessica",
-  isAwesome: true,
-  isMusician: true,
-  country: "USA"
-}
-
-const jsonString = '{"name": "Alice", "age": 25, "isAdmin": true}'
-const userObj = JSON.parse(jsonString).age;
-
-console.log(userObj)
+let person = createPerson("Charlie", 35);
+console.log(person)
