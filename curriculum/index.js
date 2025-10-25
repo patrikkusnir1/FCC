021482b991135f38d5ab0c693a94fa68ea3ae461
patrikkusnir1 <78038878+patrikkusnir1@users.noreply.github.com>
2025-10-25 08:15:38 +0300

    learn about object constructor

--- curriculum/index.js
+++ curriculum/index.js
@@ -1,7 +1,20 @@
-const originalPerson = {name: "John", age: 30};
-const copiedPerson = originalPerson;
+const num = 42;
+const numObj = Object(num);
 
-originalPerson.age = 31;
-console.log(copiedPerson.age)
+const newObj = new Object(undefined);
 
+function toObject(value) {
+  if (value === null || value === undefined) {
+    return {};
+  }
 
+  if (typeof value === "object") {
+    return value;
+  }
+
+  return Object(value)
+}
+
+console.log(toObject(null))
+console.log(toObject(true))
+console.log(toObject([1, 2, 3]))
