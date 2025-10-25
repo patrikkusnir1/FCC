856bc54c2a5dd0d4d2d8c5a57997937739ff8cf6
patrikkusnir1 <78038878+patrikkusnir1@users.noreply.github.com>
2025-10-25 08:38:32 +0300

    learn object json parse and stringify

--- curriculum/index.js
+++ curriculum/index.js
@@ -1,2 +1,19 @@
-import data from "./example.json" assert { type: "json"};
-console.log(data["list of courses"])
+const user = {
+  name: "John",
+  age: 30,
+  isAdmin: true
+}
+
+// const jsonString = JSON.stringify(user);
+
+const developerObj = {
+  firstName: "Jessica",
+  isAwesome: true,
+  isMusician: true,
+  country: "USA"
+}
+
+const jsonString = '{"name": "Alice", "age": 25, "isAdmin": true}'
+const userObj = JSON.parse(jsonString).age;
+
+console.log(userObj)
