65440c27574f3d95e0ac74616edaea0f0d54d236
patrikkusnir1 <78038878+patrikkusnir1@users.noreply.github.com>
2025-10-25 08:27:53 +0300

    learn working with json and access values

--- curriculum/index.js
+++ curriculum/index.js
@@ -1,20 +1,2 @@
-const num = 42;
-const numObj = Object(num);
-
-const newObj = new Object(undefined);
-
-function toObject(value) {
-  if (value === null || value === undefined) {
-    return {};
-  }
-
-  if (typeof value === "object") {
-    return value;
-  }
-
-  return Object(value)
-}
-
-console.log(toObject(null))
-console.log(toObject(true))
-console.log(toObject([1, 2, 3]))
+import data from "./example.json" assert { type: "json"};
+console.log(data["list of courses"])
