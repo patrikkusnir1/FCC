00f6eec3c3ca559bf08d0a38e10e3830a6b77b1e
patrikkusnir1 <78038878+patrikkusnir1@users.noreply.github.com>
2025-10-26 11:39:29 +0300

    learn for of loop

--- curriculum/index.js
+++ curriculum/index.js
@@ -1,3 +1,20 @@
-for (let i = 0; i < 5; i++) {
-  console.log(i)
+const numbers = [1, 2, 3, 4, 5];
+for (let num of numbers) {
+  // console.log(num)
+  num = num + 1;
+}
+
+// const str = "freeCodeCamp";
+// for (let char of str) {
+//   console.log(char)
+// }
+
+const people = [
+  { name: "John", age: 30 },
+  { name: "Jane", age: 25 },
+  { name: "Jim", age: 40 }
+];
+
+for (const person of people) {
+  console.log(`${person.name} is ${person.age} old.`)
 }
